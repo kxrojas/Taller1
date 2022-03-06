7dc91a6af75449369b93d964cc5474342194c44f
JoseSalcedo2 <jmsalcedo@unbosque.edu.co>
2022-03-06 16:24:16 -0500

    3 y 4

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -16,8 +16,15 @@ public class Controller {
         v = new Vista();
         v.mostrarMensaje("El proceso de carga del archivo ha finalizado");
         r = new Requerimientos();
+
         r.sumTotalSales(readCSV.getDatos());
         v.mostrarMensaje("El total de ventas es: $" + String.valueOf(r.sumTotalSales(readCSV.getDatos())));
-        }
+
+        String invoiceNo = String.valueOf(v.leerDato("Ingrese el número de factura del cual desea conocer su detalle: "));
+        v.mostrarMensaje(r.findByInvoiceNo(invoiceNo, readCSV.getDatos()));
+
+        String stockCode = v.leerString("Ingrese el código de stock para conocer las unidades vendidas: ");
+        v.mostrarMensaje(String.valueOf(r.countByStockCode(stockCode, readCSV.getDatos()) + "unidades vendidas para el stock " + stockCode));
     }
+}
 
--- src/model/Requerimientos.java
+++ src/model/Requerimientos.java
@@ -6,13 +6,49 @@ public class Requerimientos {
 
     private ReadCSV readCSV = new ReadCSV();
 
-    public double sumTotalSales(ArrayList<Datos> datos) {
+    public double sumTotalSales(ArrayList<Datos> datos){
         double total = 0.0;
-        int length = datos.size() / 9;
-        for (int i = 0; i < length; i++) {
-            total += (datos.get(i).getQuantity() * datos.get(i).getUnitPrice());
+        for (Datos dato : datos) {
+            total += (dato.getQuantity() * dato.getUnitPrice());
         }
         return total;
     }
 
+    public String findByInvoiceNo (String invoiceNo, ArrayList<Datos> datos){
+        int invNo = Integer.parseInt(invoiceNo);
+        String resp = "";
+
+        for (int i = 0; i < datos.size(); i++) {
+            int buscar = datos.get(i).getInvoiceNo();
+            if (invNo == buscar){
+                String factura = ("Invoice No:        " + datos.get(i).getInvoiceNo()
+                        +"\nStock Code:     "+ datos.get(i).getStockCode()
+                        +"\nDescription:    "+ datos.get(i).getDescription()
+                        +"\nQuantity:       "+ datos.get(i).getQuantity()
+                        +"\nInvoice Date:   "+ datos.get(i).getInvoiceDate()
+                        +"\nUnit Price:     "+ datos.get(i).getUnitPrice()
+                        +"\nCostumer ID:    "+ datos.get(i).getCostumerID()
+                        +"\nCountry:        "+ datos.get(i).getCountry());
+
+                resp = "El detalle de la factura solicitada (" +invoiceNo+ ") es\n\n" + factura;
+            } else {
+                resp = "El número ingresado no coincide con ninguna factura";
+            }
+        }
+        return resp;
+    }
+
+    public int countByStockCode(String stockCode, ArrayList<Datos> datos){
+        stockCode = stockCode.toUpperCase();
+        int contador = 0;
+        String buscar = "";
+
+        for (int i = 0; i < datos.size(); i++){
+            buscar =  datos.get(i).getStockCode();
+            if (stockCode.equals(buscar)){
+                contador += datos.get(i).getQuantity();
+            }
+        }
+        return contador;
+    }
 }
